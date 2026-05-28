2b5a743769f2798350da21c7107413cbb5bb644a
Max Winokan <max.winokan@lilly.com>
2026-05-28 14:51:27 +0100

    1.8 restyle debug and increment_progress_field

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mrich",
-    version="1.7",
+    version="1.8",
     author="Max Winokan",
     author_email="mwinokan@me.com",
     description="Pretty-printing wrappers around Rich",
